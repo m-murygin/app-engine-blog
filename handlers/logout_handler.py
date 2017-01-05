6bd72be242a75a899df29282fb5446eb982d3ab7
Maksim Murygin <muriginm@gmail.com>
2017-01-05 23:16:17 +0700

    correct logout

--- handlers/logout_handler.py
+++ handlers/logout_handler.py
@@ -3,6 +3,7 @@ from modules.auth import USER_COOKIE
 
 
 class LogoutHandler(BaseHandler):
-    def post(self):
+    def get(self):
         self.response.delete_cookie(USER_COOKIE)
         self.response.set_status(200)
+        self.redirect('/')
